c49791635b72aff38cc103c4b5721b75dad29df6
Nicu Tofan <nicu.tofan@gmail.com>
2015-03-06 19:16:10 +0200

    debug messages off by default

--- linkedlist.cmake
+++ linkedlist.cmake
@@ -1,6 +1,6 @@
 
 # enable/disable cmake debug messages related to this pile
-set (LINKEDLIST_DEBUG_MSG ON)
+set (LINKEDLIST_DEBUG_MSG OFF)
 
 # make sure support code is present; no harm
 # in including it twice; the user, however, should have used
